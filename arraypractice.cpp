71333f7342da99a3c5bb7df0d3921a4d495c8fae
Aarushi Jain <aarushi1612002@gmail.com>
2023-06-12 10:25:58 +0530

    .

---
+++ arraypractice.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // declare
+    int array[15];
+    // accessing array
+    cout << "value at index 1 is" << array[1] <<endl;
+    return 0;
+}
