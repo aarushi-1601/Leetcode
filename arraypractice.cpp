4b39c23d93b176784b7551a2308db662cd4e98a7
Aarushi Jain <aarushi1612002@gmail.com>
2023-06-12 10:28:52 +0530

    .

--- arraypractice.cpp
+++ arraypractice.cpp
@@ -6,5 +6,10 @@ int main(){
     int array[15];
     // accessing array
     cout << "value at index 1 is" << array[1] <<endl;
+    //initialsing an array
+    int second[4]={1,2,3,4};
+    //accessing an element
+    cout << "value at 2 index is " << second[2] << endl;
+
     return 0;
 }
