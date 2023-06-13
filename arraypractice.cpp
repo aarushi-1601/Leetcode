f5247ba048cb5390452785be2c22dab48f91a757
Aarushi Jain <aarushi1612002@gmail.com>
2023-06-13 20:35:18 +0530

    .

--- arraypractice.cpp
+++ arraypractice.cpp
@@ -15,6 +15,14 @@ int main()
 
     //accessing an element
     cout << "value at 2 index is " << second[2] << endl;
+    int third[15] = {2,7};
+    int n=15;
+    cout<<"printing the array"<< endl;
+    //print the array
+    for(int i=0; i<n; i++) {
+        cout<< third[i] << "";
+    
+    }
 
     return 0;
 }
