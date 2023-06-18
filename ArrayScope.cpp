953fcf2a9322f585334a9fb0afca5d1e4593e6cc
Aarushi Jain <aarushi1612002@gmail.com>
2023-06-18 20:21:36 +0530

    .

---
+++ ArrayScope.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void update(int arr[], int n) {
+
+}
+
+int main() {
+    
+    int arr[3] = {1,2,3};
+
+    update(arr, 3);
+
+
+
+
+
+
+    return 0;
+}
