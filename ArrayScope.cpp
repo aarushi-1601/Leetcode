df8b969ce5ed5d94f810fbf134a2e72a2dfdbc6a
Aarushi Jain <aarushi1612002@gmail.com>
2023-06-22 12:30:39 +0530

    .

--- ArrayScope.cpp
+++ ArrayScope.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 void update(int arr[], int n) {
 
+    cout<<"inside the function" <<endl;
+
+    for(int i=0; i<3; i++) {
+        cout<< arr[i] <<" " ;
+    } cout<<endl;
+
+
+    cout<<"going back to the main function " ;
+
 }
 
 int main() {
@@ -10,11 +19,12 @@ int main() {
     int arr[3] = {1,2,3};
 
     update(arr, 3);
+    //printing the array
 
-
-
-
-
+    for(int i=0; i<3; i++){
+        cout << arr[i] <<" ";
+    }
+    cout <<endl;
 
     return 0;
 }
