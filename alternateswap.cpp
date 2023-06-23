d6dedddf6683cdec23d9c24b35e6f6210bb6098a
Aarushi Jain <aarushi1612002@gmail.com>
2023-06-24 00:05:40 +0530

    .

--- alternateswap.cpp
+++ alternateswap.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 void printArray(int arr[], int n){
 
-    
+    for(int i =0; i<n; i++){
+        cout << arr[i] <<" ";
+    }cout << endl;
 }
+
 void alternateswap(int arr[], int size){
 
     for(int i=0; i<size; i+=2){
@@ -14,4 +17,14 @@ void alternateswap(int arr[], int size){
 
     }
     
+}
+
+int main(){
+    int even[8] = {0,2,4,6,8,10,12,14};
+    int odd[5] = {100,200,300,400,500};
+
+    alternateswap(even,8);
+    printArray(even, 8);
+
+    return 0;
 }
