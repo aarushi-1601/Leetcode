f71196693decf1740960d6bb57671dc48d7aea54
Aarushi Jain <aarushi1612002@gmail.com>
2023-06-23 23:59:13 +0530

    .

---
+++ alternateswap.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int n){
+
+    
+}
+void alternateswap(int arr[], int size){
+
+    for(int i=0; i<size; i+=2){
+        if(i+1 < size){
+            swap(arr[i], arr[i+1]);
+        }
+
+    }
+    
+}
