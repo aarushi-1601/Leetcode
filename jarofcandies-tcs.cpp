07ce3311cdbaf26600db3cce9a9760d576bfe2ae
Aarushi Jain <aarushi1612002@gmail.com>
2023-06-28 14:03:02 +0530

    .

--- jarofcandies-tcs.cpp
+++ jarofcandies-tcs.cpp
@@ -4,13 +4,16 @@ using namespace std;
 int main(){
 
     int N=10, k;
+    cout<<"enter the number of candies to be sold:"<<endl;
     cin>>k;
 
     if(k==0){
-        printf("invalid input");
+        cout<<"invalid input"<< endl;
+        cout<<"Available candies are "<< N;
     }
     else{
-        printf("candies");
+        cout<<"no of candies sold "<< k << endl;
+        cout<<"no of condies available "<< N-k << endl;
     }
 
 }
