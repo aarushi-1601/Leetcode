2680066b2cef49da233d133d65a948032367e0ba
Aarushi Jain <aarushi1612002@gmail.com>
2023-06-28 13:58:21 +0530

    .

---
+++ jarofcandies-tcs.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int N=10, k;
+    cin>>k;
+
+    if(k==0){
+        printf("invalid input");
+    }
+    else{
+        printf("candies");
+    }
+
+}
