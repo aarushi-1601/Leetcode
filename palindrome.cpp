a86c2142fab3c02a79e7c8d29eda62e06ba03577
Aarushi Jain <aarushi1612002@gmail.com>
2023-06-30 11:18:59 +0530

    .

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,28 +1,31 @@
-// get the length oflargest palindromic sebsequrnce in a given string.
+// get the length of largest palindromic subsequrnce in a given string.
 
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-class palindrome {
-public:
-   int longestPalindrome(string s) {
-      unordered_map<char, int> mp;
-      for (auto i : s)
-         mp[i]++;
-      int ma = 0, c = 0, ans = 0;
-      for (auto i : mp) {
-         if ((i.second) % 2 == 1)
-            ma++;
-         c += i.second;
+class Solution {
+   public:
+   int longestPalindromeSubseq(string s) {
+      string x = s;
+      reverse(x.begin(), x.end());
+      int n = s.size();
+      if(!n) return 0;
+      s = " " + s;
+      x = " " + x;
+      int ret = 0;
+      vector < vector <int> > dp(n + 1, vector <int>(n + 1));
+      for(int i = 1; i <= n; i++){
+         for(int j = 1; j <= n ; j++){
+            dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+            if(x[i] == s[j]) {
+               dp[i][j] = max(dp[i][j], 1 + dp[i - 1][j - 1]);
+            }
+         }
       }
-      if (ma > 0)
-         ma--;
-      ans = c - ma;
-      return ans;
+      return dp[n][n];
    }
 };
 main(){
-   palindrome ob;
-   cout << (ob.longestPalindrome("ABCDefghhgfeDmomDe"));
+   Solution ob;
+   cout << (ob.longestPalindromeSubseq("ABCDefghhgfeDmomDe"));
 }
-
