1b834d61244f4ddcc291579bdc1babb37933e74b
Aarushi Jain <aarushi1612002@gmail.com>
2023-06-30 16:02:00 +0530

    Update 30juneQ2.cpp

---
+++ 30juneQ2.cpp
@@ -0,0 +1,26 @@
+//Create a dynammic array and print the first half is asc order and second half in desc order.
+//i/p- (1,2,3,4,5)    o/p-(1,2,3,5,4)
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void printorder(int arr[], int n){
+
+    sort(arr, arr+n);
+
+    for(int i=0, i< n/2; i++)
+      cout<<arr[i]<<"";
+
+    for(int j=n-1; j>=n/2; j--)
+      cout<<arr[j]<<"";
+}
+
+int main()
+{
+    int arr[] = { 1,2,3,4,5 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printOrder(arr, n);
+ 
+    return 0;
+}
