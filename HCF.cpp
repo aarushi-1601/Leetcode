ead16560d9ed5f85cb7f115c93a245ce2ac76229
Aarushi Jain <aarushi1612002@gmail.com>
2023-07-02 17:11:39 +0530

    .

--- HCF.cpp
+++ HCF.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-
-
-
-
-
-int main(){
-
+ 
+int gcd(int a, int b)
+{
+    int result = min(a, b);
+    while (result > 0) {
+        if (a % result == 0 && b % result == 0) {
+            break;
+        }
+        result--;
+    }
+ 
+    return result;
+}
+ 
+int main()
+{
+    int a = 98, b = 56;
+    cout << "GCD of " << a << " and " << b << " is "
+         << gcd(a, b);
+    return 0;
 }
