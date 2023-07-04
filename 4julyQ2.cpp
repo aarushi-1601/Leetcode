36f9536e746f36a5d452204a69487a93fc39a91b
Aarushi Jain <aarushi1612002@gmail.com>
2023-07-04 15:40:07 +0530

    .

---
+++ 4julyQ2.cpp
@@ -0,0 +1,11 @@
+// Take an array and print 'k' number whose subsets whose sum is equal to our given input
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+}
