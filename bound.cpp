0420900fbf7f349cb73f31422b2cce6f858c08e8
Aarushi Jain <aarushi1612002@gmail.com>
2023-07-11 13:04:08 +0530

    .

--- bound.cpp
+++ bound.cpp
@@ -55,9 +55,9 @@ int lastOcc(int arr[], int n, int key){
 
 int main(){
 
-    int even[5] = {1,2,3,3,5};
+    int even[7] = {3,2,3,3,3,3,5};
 
-    cout<< "First occurence of 3 is at Index: "<< firstOcc(even, 5, 3)<< endl;
-    cout<< "Last occurence of 3 is at Index: "<< lastOcc(even, 5, 3)<< endl;
+    cout<< "First occurence of 3 is at Index: "<< firstOcc(even, 7, 3)<< endl;
+    cout<< "Last occurence of 3 is at Index: "<< lastOcc(even, 7, 3)<< endl;
     return 0;
 }
