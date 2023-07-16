39c7bed65eac281dc6c17428dc9cedbe46c1ce9f
Aarushi Jain <aarushi1612002@gmail.com>
2023-07-16 19:03:36 +0530

    .

--- arrayreverse.cpp
+++ arrayreverse.cpp
@@ -2,4 +2,43 @@
 #include <vector>
 using namespace std;
 
-vector <int>
+vector <int> reverse(vector<int> v){
+
+    int s = 0, e = v.size()-1;
+
+    while (s<=e) {
+        swap(v[s],v[e]);
+        s++;
+        e--;
+    } 
+
+    return v;
+
+}
+
+
+void print(vector<int> v){
+
+    for(int i=0; i<v.size(); i++){
+        cout<< v[i] << " ";
+    }
+
+    cout << endl;
+}
+
+    int main() {
+
+        vector<int> v;
+
+        v.push_back(11);
+        v.push_back(100);
+        v.push_back(12);
+        v.push_back(200);
+
+        vector<int> ans = reverse(v);
+
+        cout<< "Printing the reverse array" << endl;
+        print(ans);
+
+        return 0;
+    }
