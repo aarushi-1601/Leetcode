a6f2e39ca8c54ce6b6ace694465ff8f6e41fa7c9
Aarushi Jain <aarushi1612002@gmail.com>
2023-07-26 15:36:23 +0530

    .

--- 2D_Array.cpp
+++ 2D_Array.cpp
@@ -12,9 +12,14 @@ int main(){
         }
     }
 
+//print
+    for(int i=0; i<3; i++){
+        for(int j=0; j<4; j++){
+            cout<< arr[i][j]<<" ";
+        }
 
-
-
+        cout<<endl;
+    }
 
     return 0;
 }
