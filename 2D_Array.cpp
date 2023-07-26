cd36b4a6cc3c1b6f2e4f8e8a2d1365fc3ce3c525
Aarushi Jain <aarushi1612002@gmail.com>
2023-07-26 10:59:27 +0530

    .

---
+++ 2D_Array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //create 2D array
+    int arr[3][4];
+
+    for(int i=0; i<3; i++){
+        for(int j=0; j<4; j++){
+            cin>> arr[i][j];
+        }
+    }
+
+
+
+
+
+    return 0;
+}
