4e727cb1de1a7a0908b9c0599928ce8251369641
Aarushi Jain <aarushi1612002@gmail.com>
2023-07-26 15:44:44 +0530

    .

--- 2D_Array.cpp
+++ 2D_Array.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 using namespace std;
 
+bool ispresent(int arr[3][4], int target, int i, int j){
+for(int i=0; i<3; i++){
+        for(int j=0; j<4; j++){
+            if(arr[i][j]== target){
+                return 1;
+            }
+        }
+}
+return 0;
+}
+
 int main(){
 
     //create 2D array
     int arr[3][4];
 
+// idher row wise input liya hai
     for(int i=0; i<3; i++){
         for(int j=0; j<4; j++){
             cin>> arr[i][j];
@@ -19,6 +31,19 @@ int main(){
         }
 
         cout<<endl;
+    }
+
+    //search krna hai element
+
+    cout<<"Enter the element to search" << endl;
+    int target;
+    cin>>target;
+
+    if(ispresent(arr,target,3,4)){
+        cout<< "Element found"<<endl;
+    }
+    else{
+        cout<<"Not found"<<endl;
     }
 
     return 0;
