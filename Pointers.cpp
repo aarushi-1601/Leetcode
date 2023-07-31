927f86b577b4ae14e93a72d855b44a7aca41a1f3
Aarushi Jain <aarushi1612002@gmail.com>
2023-07-31 14:35:19 +0530

    .

---
+++ Pointers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num=5;
+
+    cout<< num<< endl;
+
+    //Address operator- &
+
+    cout<<"The address of num is"<< &num<< endl;
+
+    return 0;
+}
