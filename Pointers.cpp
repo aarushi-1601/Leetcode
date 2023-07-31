9610c18880b4fadcdd95a32a775bd9fa1fe4f1f6
Aarushi Jain <aarushi1612002@gmail.com>
2023-07-31 15:24:10 +0530

    .

--- Pointers.cpp
+++ Pointers.cpp
@@ -15,5 +15,11 @@ int main(){
     cout<<"Address is: " <<ptr <<endl;
     cout<<"Value is: " <<*ptr << endl;
 
+    double d = 4.3;
+    double*p2=&d;
+
+    cout<<"Address is: " <<p2 << endl;
+    cout<<"Value is: " << *p2<< endl;
+
     return 0;
 }
