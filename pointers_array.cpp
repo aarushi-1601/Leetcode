f39de0dac3234d1b349f6accafe9a0cc9d3a6b08
Aarushi Jain <aarushi1612002@gmail.com>
2023-08-04 16:36:58 +0530

    .

--- pointers_array.cpp
+++ pointers_array.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 int main(){
 
-    int arr[10];
+    int arr[10]={2,5,6};
      cout<<"Address of first memory block is " <<arr<<endl;
+     cout<<"Address of first memory block is " <<&arr[0]<<endl;
 
+    cout<<""<<*arr<<endl;
     return 0;
 }
