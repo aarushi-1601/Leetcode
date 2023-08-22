e8448ba5a26196283bbb5dae03b95db80a624d26
Aarushi Jain <aarushi1612002@gmail.com>
2023-08-22 15:12:10 +0530

    .

--- double_pointer.cpp
+++ double_pointer.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 int main(){
+    int i=5;
+    int* p=&i;
+    int** p2=&p;
 
+    cout<<endl << endl <<"All good"<< endl<< endl;
 
+    cout<<"printing p"<<p<<endl;
+    cout<<"address of p"<<&p<<endl;
+
+    
     return 0;
 }
