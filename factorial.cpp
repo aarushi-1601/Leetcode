8fb3648330a660ec12ae2b61a2cf9c9b5cfe79df
Aarushi Jain <aarushi1612002@gmail.com>
2023-08-23 19:54:03 +0530

    .

---
+++ factorial.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+
+//base case
+if(n==0)
+    return 1;
+
+int smallprob = factorial(n-1);
+int bigprob = n*smallprob;
+
+return bigprob;
+
+}
+
+int main(){
+    
+    int n;
+    cin>>n;
+
+    int ans=factorial(n);
+
+    cout<<ans<<endl;
+
+    return 0;
+}
