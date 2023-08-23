479a794cf9acc9fc058dbb8fafcd7b76587a16ab
Aarushi Jain <aarushi1612002@gmail.com>
2023-08-23 12:48:48 +0530

    .

--- macros.cpp
+++ macros.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 using namespace std;
 
+#define PI 3.14
+
 int main(){
-    
-    int r=5;
-    double area=3.14*r*r;
-    cout<<"Area of circle is"<<area<<endl;
+
+    int r=5;  
+    double area= PI*r*r;
+    //or double pi=3.14
+    // double area=pi*r*r;
+    cout<<"Area of circle is "<<area<<endl;
 
     return 0;
 }
